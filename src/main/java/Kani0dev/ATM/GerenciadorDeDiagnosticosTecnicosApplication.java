4f8cf37a1c07e6a4a609a27eeb1efa628a7349ac
kani0dev <allanchagasilva@gmail>
2025-12-15 20:28:45 -0300

    project inializer

---
+++ src/main/java/Kani0dev/ATM/GerenciadorDeDiagnosticosTecnicosApplication.java
@@ -0,0 +1,13 @@
+package Kani0dev.ATM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeDiagnosticosTecnicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeDiagnosticosTecnicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Kani0dev/ATM/GerenciadorDeDiagnosticosTecnicosApplicationTests.java
@@ -0,0 +1,13 @@
+package Kani0dev.ATM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeDiagnosticosTecnicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
