ddd4862ac4a4c2606664a1ffb3f40d77f91d89ac
kani0dev <allanchagasilva@gmail.com>
2026-06-29 20:12:43 -0300

    dev docker container

--- src/main/java/Kani0dev/ATM/GerenciadorDeDiagnosticosTecnicosApplication.java
+++ src/main/java/Kani0dev/ATM/GerenciadorDeDiagnosticosTecnicosApplication.java
@@ -12,9 +12,11 @@ public class GerenciadorDeDiagnosticosTecnicosApplication {
     
     public static void main(String[] args) {
         Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-        dotenv.entries().forEach(entry -> 
-            System.setProperty(entry.getKey(), entry.getValue())
-        );
+        dotenv.entries().forEach(entry -> {
+            if (System.getenv(entry.getKey()) == null) {
+                System.setProperty(entry.getKey(), entry.getValue());
+            }
+        });
         SpringApplication.run(GerenciadorDeDiagnosticosTecnicosApplication.class, args);
     }
 
