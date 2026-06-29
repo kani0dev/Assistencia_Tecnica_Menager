90ef6832b1ab12335bc1163e88220027e9f42b5b
kani0dev <allanchagasilva@gmail.com>
2026-06-29 20:04:50 -0300

    fix - tinha um dependecia de dotenv duplicada, configuraçoes no main tb estavam incorretas

--- src/main/java/Kani0dev/ATM/GerenciadorDeDiagnosticosTecnicosApplication.java
+++ src/main/java/Kani0dev/ATM/GerenciadorDeDiagnosticosTecnicosApplication.java
@@ -5,11 +5,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class GerenciadorDeDiagnosticosTecnicosApplication {
-
+    
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> 
+            System.setProperty(entry.getKey(), entry.getValue())
+        );
         SpringApplication.run(GerenciadorDeDiagnosticosTecnicosApplication.class, args);
     }
 
