8c247754b1fa557abc4090bf9d69686028259ed2
kani0dev <allanchagasilva@gmail.com>
2026-06-29 19:23:51 -0300

    bug, spring is not reading the .env

--- src/main/java/Kani0dev/ATM/GerenciadorDeDiagnosticosTecnicosApplication.java
+++ src/main/java/Kani0dev/ATM/GerenciadorDeDiagnosticosTecnicosApplication.java
@@ -1,13 +1,24 @@
 package Kani0dev.ATM;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class GerenciadorDeDiagnosticosTecnicosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GerenciadorDeDiagnosticosTecnicosApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciadorDeDiagnosticosTecnicosApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner logDatabaseUrl(Environment env) {
+        return args -> {
+            var url = env.getProperty("spring.datasource.url");
+            System.out.println("=== Conexao com o banco: " + url + " ===");
+        };
+    }
 
 }
